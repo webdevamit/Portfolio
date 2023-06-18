c9aa888124c20ca9521b2999abc6c02c6b22d87c
Amit Chauhan <riccky7@gmail.com>
2023-06-18 22:51:29 +0530

    React portfolio

---
+++ src/content.js
@@ -0,0 +1,190 @@
+const logotext = "Amit Chauhan";
+const meta = {
+  title: "Amit Chauhan",
+  description:
+    "I’m Amit Chauhan Senior FrontEnd devloper,currently working in Natwest Group",
+};
+
+const introdata = {
+  title: "I’m Amit Chauhan",
+  animated: {
+    first: "I am a passionate FrontEnd developer",
+    second:
+      "I am building eCommerce and Currency Trading web application since years",
+    third: "I am a proud father",
+  },
+  description:
+    "Innovative developer offering 8 years of expertise in web technologies. Skilled in directing development with creative and performance-oriented approach. Quickly learns and master’s new technologies while working in both team and independent settings. Having huge experience in frontend development and has developed a passion for coding, primarily using React. Experience in working Ecommerce based applications and making responsive web application.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+  title: "About my self",
+  aboutme:
+    "Proud father who is building frontend application since years of experience. Driven by a passion for continuous learning, Stays up to date with the latest industry trends and emerging technologies. Always eager to explore new tools and techniques that can elevate my work and push the boundaries of what is possible in frontend development. This dedication to staying at the cutting edge enables myself to deliver innovative and forward-thinking solutions.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Technical Lead",
+    where: "Natwest Group",
+    date: "2022 to current",
+  },
+  {
+    jobtitle: "Specialist software engineer",
+    where: "Larsen & Toubro Infotech",
+    date: "2020-2021",
+  },
+  {
+    jobtitle: "Project Manager",
+    where: "Webkul Software Pvt Ltd",
+    date: "2015-2020",
+  },
+];
+
+const skills = [
+  {
+    name: "JavaScript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 90,
+  },
+  {
+    name: "Redux",
+    value: 60,
+  },
+  {
+    name: "Redux Thunk",
+    value: 85,
+  },
+  {
+    name: "TypeScript",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "luckyamy000@gmail.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/webdevamit",
+  twitter: "https://twitter.com/webdevchauhan",
+  facebook: "https://www.facebook.com/amy202020",
+  linkedin: "https://www.linkedin.com/in/amit-chauhan-a2a26290/",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
